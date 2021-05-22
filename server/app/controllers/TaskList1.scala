6ab0e54dd7ad5cc48245e82bc19d112d32e9ea4c
lmartinez <lmartinezj@gmail.com>
2021-05-22 17:16:35 -0500

    login1.scala.html: Added login template page TaskList1.scala: Added methods for rendering login page and validating login submit with GET

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -6,6 +6,15 @@ import play.api.i18n._
 
 @Singleton
 class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def login = Action {
+    Ok(views.html.login1())
+  }
+
+  def validateLoginGet(username: String, password: String) = Action {
+    Ok(s"username: $username is logged in with password: $password")
+  }
+
   def taskList = Action {
     val tasks = List("task1", "task3", "task3", "eat", "sleep")
     Ok(views.html.taskList1(tasks))
