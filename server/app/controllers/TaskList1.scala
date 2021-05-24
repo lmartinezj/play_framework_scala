aaa96db96a0b8fc7d35115906033ba9cc619040a
lmartinez <lmartinezj@gmail.com>
2021-05-24 17:56:36 -0500

    TaskList1: Added implicit request to login action taskList1.scala.html: Added request header parameter routes: removed + nocsrf

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -8,7 +8,7 @@ import play.api.i18n._
 @Singleton
 class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def login = Action {
+  def login = Action { implicit request =>
     Ok(views.html.login1())
   }
 
