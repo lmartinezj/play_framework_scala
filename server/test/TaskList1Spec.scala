021dd439152bc8e3a6aa5f42545c463365775c9b
lmartinez <lmartinezj@gmail.com>
2021-06-07 12:00:08 -0500

    TaskList1Spec.scala: added ccsSelector to find <h2>, added id to find pwdField, added check for PageTitle on rendered TaskList page, added check for <ul><li>'s on initial hard coded list for default user

--- server/test/TaskList1Spec.scala
+++ server/test/TaskList1Spec.scala
@@ -5,14 +5,19 @@ class TaskList1Spec extends PlaySpec with GuiceOneServerPerSuite with OneBrowser
   "Task List 1" must {
     "login and access functions" in {
       go to s"http://localhost:$port/login1"
+      pageTitle mustBe "Login"
+      find(cssSelector("h2")).isEmpty mustBe false
+      find(cssSelector("h2")).foreach(_.text mustBe "Login")
+      click on "username-login"
+      textField("username-login").value = "luis"
+      click on "password-login"
+      pwdField(id("password-login")).value = "123"
+      submit()
       eventually {
-        find("h2").isEmpty mustBe false
-        find("h2").foreach(_.text mustBe "Login")
-        click on "username-login"
-        textField("username-login").value = "luis"
-        click on "password-login"
-        pwdField("password-login").value = "123"
-        submit()
+        pageTitle mustBe "Task List"
+        find(cssSelector("h2")).isEmpty mustBe(false)
+        find(cssSelector("h2")).foreach(_.text mustBe "Task List")
+        findAll(cssSelector("li")).toList.map(_.text) mustBe List("Make videos", "Eat", "Sleep", "Code")
       }
     }
   }
