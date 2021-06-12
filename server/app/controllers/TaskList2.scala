e5cb473b9abb31bd674841c9c128ebc62a8fba29
lmartinez <lmartinezj@gmail.com>
2021-06-12 18:43:44 -0500

    TaskList2.scala: Added validateLoginGet action that validates user that displays default user task list if validates and goes back to login if not validated login2.scala.html: Added <h2> and event handler for onclick on createUser and login <button>'s taskList2.scala.html: Added the view to display the user task list does not include neither main partial nor Flash  routes: Added validateGet2 route that points to controllers.TaskList2.validateLoginGet version2.js: Added login() ad createUser() functions that act upon onclick events on the views form

--- server/app/controllers/TaskList2.scala
+++ server/app/controllers/TaskList2.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import models.TaskListInMemoryModel
 import play.api.mvc.{AbstractController, ControllerComponents}
 
 import javax.inject._
@@ -16,5 +17,16 @@ class TaskList2 @Inject()(cc: ControllerComponents) extends AbstractController(c
     Ok(views.html.login2())
   }
 
+  def validateLoginGet(username: String, password: String) = Action {
+
+    if (TaskListInMemoryModel.validateUser(username, password)) {
+      println("If true")
+      Ok(views.html.taskList2(TaskListInMemoryModel.getTasks(username))).withSession("username" -> username)
+    } else {
+      println("If false")
+      Ok(views.html.login2())
+    }
+    //Ok(s"username: $username is logged in with password: $password")
+  }
 
 }
--- server/public/javascript/version2.js
+++ server/public/javascript/version2.js
@@ -1,2 +1,15 @@
 
 $("#contents").load("/login2")
+
+function login() {
+    const username = $("#loginName").val()
+    const password = $("#loginPassword").val()
+    console.log(username + " Tried to login with password "  + password)
+    $("#contents").load("/validateGet2?username=" + username + "&password=" + password)
+}
+
+function createUser() {
+    const username = $("#createUsername").val()
+    const password = $("#createPassword").val()
+    console.log("Try to create user " + username + " with password " + password)
+}
