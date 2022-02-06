b23f9c3247328c598e2e64b09076c25763af4196
lmartinez <lmartinezj@gmail.com>
2022-02-06 16:33:33 -0500

    Start of Version 3 (Using JSON objects) Project needed sbt, play and twirl updates Previous commit !!!! build.sbt: Updated twirl-compiler to 1.4.9 (because of compatibilities with twirl and scala compilers) TaskList3.scala: added load implementation to render main3.scala.html main3.scala.html: Updated template to include a <div>login-section</div> and a <div>task-section</div> So we can hidde or show the sections programmatically depending on the data received from the request Updated javascript/version3.js yet to be implemented

--- server/app/controllers/TaskList3.scala
+++ server/app/controllers/TaskList3.scala
@@ -9,9 +9,11 @@ import javax.inject._
 
 @Singleton
 class TaskList3 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
   def load = Action { implicit request =>
     Ok(views.html.main3())
   }
+
   def data = Action {
     Ok(Json.toJson(Seq("a", "b", "c")))
   }
